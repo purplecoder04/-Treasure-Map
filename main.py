43af3fa3179cbfaacc0592a34142dc072fee54ff
purplecoder04 <purple_lover_04@msn.com>
2021-03-11 21:02:39 +0000

    rows were wrong

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-col= int(position[0]) -1
-row = int(position[1])-1
+col= int(position[1]) -1
+row = int(position[0])-1
 map[col][row]= "X"
 
 
